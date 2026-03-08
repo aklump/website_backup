a015882ae327bd438473cb35e3e7188f037c0c76
Aaron Klump <sourcecode@intheloftstudios.com>
2026-03-08 09:49:58 -0700

    build

--- .web_package/hooks/build/00_symfony_version.php
+++ .web_package/hooks/build/00_symfony_version.php
@@ -3,7 +3,7 @@
 /**
  * @file Set the version in the Symfony Console controller.
  */
-$controller = "bin/website_backup";
+$controller = "bin/website-backup";
 $content = file_get_contents($controller);
 $content = preg_replace('#setVersion\((.+?)\)#', 'setVersion(\'' . $argv[2] . '\')', $content);
 file_put_contents($controller, $content);
